446d0e4542a88ec937904d9d8d3bf77b757ff859
甚田翔吾 <jinta@jindenshougonoMacBook-Pro.local>
2022-03-03 09:42:24 +0900

    12

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
 const app = Vue.createApp({
   data: () => ({
-    message: 'Hello Vue.js!'
+    message: 'Hello Vue.js!',
+    count: 99,
+    user: {
+      lastName: 'Nakamura',
+      firstName: 'Yuta',
+      prefecture: 'Tokyo',
+    },
+    colors: ['red', 'green', 'blue']
   })
 })
+
 app.mount('#app')
 
