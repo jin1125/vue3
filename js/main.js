b1ea8f6a33a10ceb24bbed446fbbca8efb55c45f
jinta <shogo.jinta@qnote.co.jp>
2022-03-03 10:01:53 +0900

    16

--- js/main.js
+++ js/main.js
@@ -1,13 +1,6 @@
 const app = Vue.createApp({
   data: () => ({
     message: 'Hello Vue.js!',
-    count: 99,
-    user: {
-      lastName: 'Nakamura',
-      firstName: 'Yuta',
-      prefecture: 'Tokyo',
-    },
-    colors: ['red', 'green', 'blue']
   })
 })
 
