7eccbf89781f98450ba12342612d30bc2a8169f4
jin1125 <shogo.jinta@gmail.com>
2022-03-06 10:01:07 +0900

    finish

--- js/main.js
+++ js/main.js
@@ -1,32 +1,7 @@
-const buttonCounter = {
-  template: `<div><span>count:</span><button @click="countUp">{{ count }}</button></div>`,
-  data: () => ({
-    count: 0
-  }),
-  methods: {
-    countUp: function(event) {
-      this.count++
-    }
-  }
-}
-
 const app = Vue.createApp({
   data: () => ({
-    age: 0
+    show: true
   }),
-  methods: {
-    clickHandler: function() {
-      this.message = new Date().toLocaleTimeString()
-    }
-  },
-  computed: {
-  },
-  watch: {
-  },
-  components: {
-    'button-counter': buttonCounter
-  }
 })
 
 app.mount('#app')
-
