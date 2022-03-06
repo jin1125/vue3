ba3b1837122da15633cc556172056bac457fc853
jin1125 <shogo.jinta@gmail.com>
2022-03-06 09:45:44 +0900

    111

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
+const buttonCounter = {
+  template: `<div><span>count:</span><button @click="countUp">{{ count }}</button></div>`,
+  data: () => ({
+    count: 0
+  }),
+  methods: {
+    countUp: function(event) {
+      this.count++
+    }
+  }
+}
+
 const app = Vue.createApp({
   data: () => ({
-    counter: 0
+    age: 0
   }),
   methods: {
     clickHandler: function() {
-      this.counter++
+      this.message = new Date().toLocaleTimeString()
     }
   },
   computed: {
   },
   watch: {
   },
+  components: {
+    'button-counter': buttonCounter
+  }
 })
 
 app.mount('#app')
